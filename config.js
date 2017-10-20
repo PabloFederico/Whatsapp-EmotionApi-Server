d610f74f8bdd60082b9f48cd0028097060088678
Pablo Federico <pablo.federico@softtek.com>
2017-10-19 21:30:25 -0300

    BD Postgree creada y mas cambios

---
+++ config.js
@@ -0,0 +1,3 @@
+'use strict';
+
+exports.DATABASE_URL = process.env.DATABASE_URL || 'postgres://lfpkajtzjisazf:9e53cba581ec0335eebe68b5d99f994ed1880a1c7d979ada29915d11edcf300e@ec2-54-243-187-133.compute-1.amazonaws.com:5432/dbtmh9bu1g7l4t';
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var app = express();
+var pg = require("pg");
+var config = require('./config');
+
 
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
 
 app.get('/', function(request, response) {
   response.render('pages/index');
@@ -16,3 +19,22 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+
+pg.defaults.ssl = true;
+
+pg.connect(config.DATABASE_URL, function (err, client, done) {
+  if (err)
+    throw err;
+	
+    client.query('CREATE TABLE IF NOT EXISTS' +
+    ' prueba(id SERIAL PRIMARY KEY,' +
+    ' nombre VARCHAR(30),' +
+    ' apellido VARCHAR(30))', function (err, result) {
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+    });
+
+  });
